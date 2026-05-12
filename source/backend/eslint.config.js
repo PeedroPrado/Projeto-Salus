aa6130f34f2ac72e25f4dd8d283182e6877383fa
Pedro H Prado <pedrohenriquepradodenovaes@gmail.com>
2026-05-12 01:20:17 -0300

    chore: setup eslint prettier husky and typescript configs

---
+++ source/backend/eslint.config.js
@@ -0,0 +1,15 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+);
